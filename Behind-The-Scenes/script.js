c1abacbbbb6f2d3548809323c6972a5badd016b0
p-prime1 <preciousnwaka95@gmail.com>
2023-03-25 23:06:50 +0100

    SCOPE

--- Behind-The-Scenes/script.js
+++ Behind-The-Scenes/script.js
@@ -1,1 +1,33 @@
-'use strict';
+"use strict";
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName} You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const firstName = "Nduka";
+      const str = `Oh, and your're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+
+      const output = "NEw output";
+      console.log(output);
+    }
+    console.log(output);
+    console.log(millenial);
+    // console.log(add(2, 3));
+  }
+
+  printAge();
+  return age;
+}
+
+const firstName = "prime";
+calcAge(1991);
